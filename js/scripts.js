f5bdc6fdbc70568f476f9e06cf29f9e5005c35ab
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2017-12-10 16:39:07 +0100

    Change scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
-var span = $("span");
-    span.each(function(index, element) {
-});
 
+//zmiana CSS elementów za pomocą jQ
+
+var span = $("span");
+	span.each(function(index, element) {
+});
 // span.each(function(index, element) {
 // 	if(index % 2 == 0) {
 // 		$(element).css('color', 'red');
 // 	};
 // });
 $("span:even").css('color', 'red');
+
+//dodawanie nowych elementów (przycisków) za pomocą jQ 
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
 	var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-$(element).append(button)
+	$(element).append(button)
 });
 
+//dodawanie event handlerów
 $("button").click(function(){
 	alert($(this).attr("data-tmp"));
 });
